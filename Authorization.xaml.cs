e566b11b8edbdd8ce07db755a0201d1feddc3579
DilyaraD <dautova565@gmail.com>
2025-02-21 21:37:14 +0300

    Добавьте файлы проекта.

---
+++ Authorization.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GeotekMetallCompleteDesktop
+{
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void RemoveText(object sender, EventArgs e)
+        {
+            TextBox instance = (TextBox)sender;
+            if (instance.Text == instance.Tag.ToString())
+            {
+                instance.Text = "";
+                instance.Foreground = Brushes.Black; 
+            }
+        }
+
+        private void AddText(object sender, EventArgs e)
+        {
+            TextBox instance = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(instance.Text))
+            {
+                instance.Text = instance.Tag.ToString();
+                instance.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string loginText = login.Text;
+            string passwordText = password.Text;
+
+            if (loginText == (string)login.Tag)
+            {
+                loginText = ""; 
+            }
+
+            if (passwordText == (string)password.Tag)
+            {
+                passwordText = ""; 
+            }
+
+            MessageBox.Show($"Логин: {loginText}\nПароль: {passwordText}");
+
+        }
+    }
+}
